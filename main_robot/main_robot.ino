7a1604e9daf4acbf50a30687e3f3cc3c47f473ad
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2022-07-12 00:16:36 +0900

    update

--- main_robot/main_robot.ino
+++ main_robot/main_robot.ino
@@ -53,19 +53,27 @@ struct Arm_pos {
 
 // 手前から
 const Arm_pos pos_out[HW] = {
-  {ARM_HAND_ZERO,        ARM_MID_ZERO - 4630, ARM_ROOT_ZERO + 1280},
-  {ARM_HAND_ZERO + 100,  ARM_MID_ZERO - 4320, ARM_ROOT_ZERO + 1180},
-  {ARM_HAND_ZERO + 350,  ARM_MID_ZERO - 4050, ARM_ROOT_ZERO + 1100},
-  {ARM_HAND_ZERO + 510,  ARM_MID_ZERO - 3780, ARM_ROOT_ZERO + 1170},
-  {ARM_HAND_ZERO + 710,  ARM_MID_ZERO - 3480, ARM_ROOT_ZERO + 1280},
-  {ARM_HAND_ZERO + 910,  ARM_MID_ZERO - 3180, ARM_ROOT_ZERO + 1390},
-  {ARM_HAND_ZERO + 1170, ARM_MID_ZERO - 2780, ARM_ROOT_ZERO + 1510},
-  {ARM_HAND_ZERO + 1400, ARM_MID_ZERO - 2380, ARM_ROOT_ZERO + 1570}
-  
+  {ARM_HAND_ZERO + 0,    ARM_MID_ZERO - 4650, ARM_ROOT_ZERO + 1000},
+  {ARM_HAND_ZERO + 100,  ARM_MID_ZERO - 4340, ARM_ROOT_ZERO + 1020},
+  {ARM_HAND_ZERO + 350,  ARM_MID_ZERO - 4040, ARM_ROOT_ZERO + 1070},
+  {ARM_HAND_ZERO + 510,  ARM_MID_ZERO - 3750, ARM_ROOT_ZERO + 1110},
+  {ARM_HAND_ZERO + 710,  ARM_MID_ZERO - 3420, ARM_ROOT_ZERO + 1200},
+  {ARM_HAND_ZERO + 910,  ARM_MID_ZERO - 3090, ARM_ROOT_ZERO + 1270},
+  {ARM_HAND_ZERO + 1170, ARM_MID_ZERO - 2740, ARM_ROOT_ZERO + 1390},
+  {ARM_HAND_ZERO + 1400, ARM_MID_ZERO - 2320, ARM_ROOT_ZERO + 1530}
 };
 const Arm_pos pos_in[HW] = {
-  
+  {ARM_HAND_ZERO + 530  ,  ARM_MID_ZERO - 3750, ARM_ROOT_ZERO + 1050},
+  {ARM_HAND_ZERO + 710,  ARM_MID_ZERO - 3460, ARM_ROOT_ZERO + 1120},
+  {ARM_HAND_ZERO + 910,  ARM_MID_ZERO - 3130, ARM_ROOT_ZERO + 1230},
+  {ARM_HAND_ZERO + 1170,  ARM_MID_ZERO - 2750, ARM_ROOT_ZERO + 1380},
+  {ARM_HAND_ZERO + 1400,  ARM_MID_ZERO - 2370, ARM_ROOT_ZERO + 1520},
+  {ARM_HAND_ZERO + 1600,  ARM_MID_ZERO - 1940, ARM_ROOT_ZERO + 1720},
+  {ARM_HAND_ZERO + 1800, ARM_MID_ZERO - 1370, ARM_ROOT_ZERO + 1930},
+  {ARM_HAND_ZERO + 2200, ARM_MID_ZERO - 400, ARM_ROOT_ZERO + 2350}
 };
+
+const Arm_pos pos_home = {ARM_HAND_ZERO + 200, ARM_MID_ZERO - 4300, ARM_ROOT_ZERO - 200};
 
 void setup() {
   Wire.begin(8);
@@ -85,29 +93,117 @@ void setup() {
   pinMode(ENA_PIN, OUTPUT);
   krs.begin();
   flip_servo.write(SERVO_RELEASE_DEG);
+  move_arm(pos_home, 100, 10);
+  //out_demo();
+  //in_demo();
+  flip_demo();
+}
+
+void flip_demo(){
+  Arm_pos pos;
+  bool is_black = true;
+  for (int i = 0; i < 8; ++i){
+    pos = pos_out[i];
+    pos.root -= 400;
+    move_arm(pos, 50, 10);
+    
+    pos.root += 400;
+    move_arm(pos, 50, 10);
+    if (is_black)
+      hold_black_out();
+    else
+      hold_white_out();
+    
+    pos.root -= 800;
+    move_arm(pos, 50, 10);
+
+    flip(is_black);
+    if (i < 7)
+      pos = pos_in[i + 1];
+    else
+      pos = pos_in[i];
+    pos.root -= 400;
+    move_arm(pos, 50, 10);
+    
+    pos.root += 400;
+    move_arm(pos, 50, 10);
+    release_in();
+
+    pos.root -= 400;
+    move_arm(pos, 50, 10);
+
+    is_black = !is_black;
+  }
+  move_arm(pos_home, 100, 10);
+}
+
+void in_demo(){
+  Arm_pos pos;
+  for (int i = 0; i < 8; ++i){
+    pos = pos_in[i];
+    pos.root -= 400;
+    move_arm(pos, 50, 10);
+    
+    pos.root += 400;
+    move_arm(pos, 50, 10);
+    release_in();
+    
+    pos.root -= 400;
+    move_arm(pos, 50, 10);
+
+    pos.root += 400;
+    move_arm(pos, 50, 10);
+    hold_black_in();
+
+    pos.root -= 400;
+    move_arm(pos, 50, 10);
+    
+  }
+  move_arm(pos_in[7], 50, 10);
+  release_in();
+  move_arm(pos_home, 100, 10);
+}
+
+void out_demo(){
   Arm_pos pos;
   for (int i = 0; i < 8; ++i){
     pos = pos_out[i];
     pos.root -= 400;
-    move_arm(pos, 30, 10);
+    move_arm(pos, 50, 10);
     
     pos.root += 400;
-    move_arm(pos, 30, 10);
+    move_arm(pos, 50, 10);
     release_out();
     
     pos.root -= 400;
-    move_arm(pos, 30, 10);
+    move_arm(pos, 50, 10);
 
     pos.root += 400;
-    move_arm(pos, 30, 10);
+    move_arm(pos, 50, 10);
     hold_black_out();
 
     pos.root -= 400;
-    move_arm(pos, 30, 10);
+    move_arm(pos, 50, 10);
     
   }
-  Arm_pos arm_pos = {ARM_HAND_ZERO + 1400, ARM_MID_ZERO - 2380, ARM_ROOT_ZERO + 1570};
-  move_arm(arm_pos, 30, 10);
+  move_arm(pos_out[7], 50, 10);
+  release_out();
+  move_arm(pos_home, 100, 10);
+}
+
+
+
+void flip(bool is_black){
+  flip_servo.write(SERVO_GRIP_DEG);
+  delay(400);
+  if (is_black)
+    hold_white_in();
+  else
+    hold_black_in();
+  delay(100);
+  release_out();
+  delay(100);
+  flip_servo.write(SERVO_RELEASE_DEG);
 }
 
 void stepper_enable() {
@@ -189,7 +285,7 @@ void arm_zero() {
 
 void demo() {
   Arm_pos arm_pos = {ARM_HAND_TEST_1, ARM_MID_TEST_1, ARM_ROOT_TEST_1 - 400};
-  move_arm(arm_pos, 30, 10);
+  move_arm(arm_pos, 50, 10);
   stepper_enable();
   stepper_move(true, 2000, STEPPER_DELAY_FAST);
   delay(1000);
@@ -197,14 +293,14 @@ void demo() {
   stepper_disable();
 
   arm_pos.root += 400;
-  move_arm(arm_pos, 30, 10);
+  move_arm(arm_pos, 50, 10);
 
   flip_servo.write(SERVO_RELEASE_DEG);
   hold_black_out();
   delay(500);
 
   arm_pos.root -= 400;
-  move_arm(arm_pos, 30, 10);
+  move_arm(arm_pos, 50, 10);
 
   flip_servo.write(SERVO_GRIP_DEG);
   delay(100);
@@ -218,29 +314,29 @@ void demo() {
   arm_pos.mid = ARM_MID_TEST_2;
   arm_pos.hand = ARM_HAND_TEST_2;
   arm_pos.root =  ARM_ROOT_TEST_2 - 200;
-  move_arm(arm_pos, 30, 10);
+  move_arm(arm_pos, 50, 10);
 
   arm_pos.root =  ARM_ROOT_TEST_2;
-  move_arm(arm_pos, 30, 10);
+  move_arm(arm_pos, 50, 10);
 
   delay(300);
 
   release_in();
 
   arm_pos.root -= 400;
-  move_arm(arm_pos, 30, 10);
+  move_arm(arm_pos, 50, 10);
 
 
 
   arm_pos = {ARM_HAND_TEST_1, ARM_MID_TEST_1, ARM_ROOT_TEST_1};
-  move_arm(arm_pos, 30, 10);
+  move_arm(arm_pos, 50, 10);
 
   flip_servo.write(SERVO_RELEASE_DEG);
   hold_white_out();
   delay(500);
 
   arm_pos.root -= 400;
-  move_arm(arm_pos, 30, 10);
+  move_arm(arm_pos, 50, 10);
 
   flip_servo.write(SERVO_GRIP_DEG);
   delay(100);
@@ -254,34 +350,37 @@ void demo() {
   arm_pos.mid = ARM_MID_TEST_2;
   arm_pos.hand = ARM_HAND_TEST_2;
   arm_pos.root =  ARM_ROOT_TEST_2 - 200;
-  move_arm(arm_pos, 30, 10);
+  move_arm(arm_pos, 50, 10);
 
   arm_pos.root =  ARM_ROOT_TEST_2;
-  move_arm(arm_pos, 30, 10);
+  move_arm(arm_pos, 50, 10);
 
   delay(300);
 
   release_in();
 
   arm_pos.root -= 400;
-  move_arm(arm_pos, 30, 10);
+  move_arm(arm_pos, 50, 10);
 }
 
-#define NOT_RECEIVED -1
+bool received = false;
 
-int received = NOT_RECEIVED;
+Arm_pos global_pos;
 
 void demo_receive() {
   if (Wire.available()) {
-    received = Wire.read();
+    global_pos.hand = ARM_HAND_ZERO + ((int)Wire.read() * 256 + (int)Wire.read()) - 10000;
+    global_pos.mid = ARM_MID_ZERO + ((int)Wire.read() * 256 + (int)Wire.read()) - 10000;
+    global_pos.root = ARM_ROOT_ZERO + ((int)Wire.read() * 256 + (int)Wire.read()) - 10000;
+    received = true;
   }
 }
 
 void loop() {
-  /*
-  if (received != NOT_RECEIVED){
-    demo();
-    received = NOT_RECEIVED;
+  if (received){
+    Arm_pos reset_pos = {krs.getPos(ARM_HAND), krs.getPos(ARM_MID), ARM_ROOT_ZERO - 200};
+    move_arm(reset_pos, 50, 10);
+    move_arm(global_pos, 50, 10);
+    received = false;
   }
-  */
 }
