a42f2cdf7344bd8b95202888e95f2a4d9b742e68
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2022-07-12 13:42:48 +0900

    now you can play against robot

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import subprocess
 import serial
 from othello_py import *
 from random import choice
+import tkinter
 
 
 robot = serial.Serial('COM10',115200, timeout=1000000)
@@ -16,31 +17,43 @@ def send_cmds(cmds):
         response = robot.read()
         print(response)
 
-''' demo
-for cmd in ['400', '023', '333']:
-    print(cmd)
-    cmd += '\n'
-    robot.write(cmd.encode())
-    print(robot.read(3))
-    response = robot.read()
-    print(response)
-'''
+ai_player = 0
 
+# 定数
+offset_y = 10
+offset_x = 10
+rect_size = 60
+circle_offset = 3
 
 o = othello()
-while True:
-    if not o.check_legal():
-        o.player = 1 - o.player
-        if not o.check_legal():
-            break
-    '''
-    legal_list = []
-    for y in range(hw):
-        for x in range(hw):
-            if o.grid[y][x] == legal:
-                legal_list.append([y, x])
-    policy = choice(legal_list)
-    '''
+legal_buttons = []
+
+# GUI部分
+app = tkinter.Tk()
+app.geometry('500x700')
+app.title('オセロAIの教科書 サンプルプログラム')
+canvas = tkinter.Canvas(app, width=500, height = 700)
+pixel_virtual = tkinter.PhotoImage(width=1, height=1)
+
+def on_closing():
+    egaroucid.kill()
+    app.destroy()
+
+app.protocol("WM_DELETE_WINDOW", on_closing)
+
+# 盤面の作成
+for y in range(hw):
+    for x in range(hw):
+        canvas.create_rectangle(offset_x + rect_size * x, offset_y + rect_size * y, offset_x + rect_size * (x + 1), offset_y + rect_size * (y + 1), outline='black', width=2, fill='#16a085')
+
+# 石数表示
+stone_str = tkinter.StringVar()
+stone_str.set('*● 2 - 2 ○ ')
+stone_label = tkinter.Label(canvas, textvariable=stone_str, font=('', 50))
+stone_label.place(x=250, y=600, anchor=tkinter.CENTER)
+
+def ai():
+    global clicked
     grid_str = str(o.player) + '\n'
     for yy in range(hw):
         for xx in range(hw):
@@ -68,3 +81,94 @@ while True:
     o.move(policy[0], policy[1])
     print(policy)
     o.print_info()
+    if not o.check_legal():
+        o.player = 1 - o.player
+        if not o.check_legal():
+            o.print_info()
+            egaroucid.kill()
+            o.player = -1
+            print('終局しました')
+    s = ''
+    if o.player == 0:
+        s += '*'
+    else:
+        s += ' '
+    s += '● '
+    s += str(o.n_stones[0])
+    s += ' - '
+    s += str(o.n_stones[1])
+    s += ' ○'
+    if o.player == 1:
+        s += '*'
+    else:
+        s += ' '
+    stone_str.set(s)
+    o.print_info()
+    show_grid()
+
+def get_coord(event):
+    global clicked
+    y = int(event.widget.cget('text')[0])
+    x = int(event.widget.cget('text')[2])
+    print(y, x)
+    clicked = True
+    o.move(y, x)
+    if not o.check_legal():
+        o.player = 1 - o.player
+        if not o.check_legal():
+            o.print_info()
+            ai_exe.kill()
+            o.player = -1
+            print('終局しました')
+    s = ''
+    if o.player == 0:
+        s += '*'
+    else:
+        s += ' '
+    s += '● '
+    s += str(o.n_stones[0])
+    s += ' - '
+    s += str(o.n_stones[1])
+    s += ' ○'
+    if o.player == 1:
+        s += '*'
+    else:
+        s += ' '
+    stone_str.set(s)
+    o.print_info()
+    show_grid()
+
+def show_grid():
+    global clicked, legal_buttons
+    for button in legal_buttons:
+        button.place_forget()
+    legal_buttons = []
+    for y in range(hw):
+        for x in range(hw):
+            try:
+                canvas.delete(str(y) + '_' + str(x))
+            except:
+                pass
+            if o.grid[y][x] == vacant:
+                continue
+            color = ''
+            if o.grid[y][x] == black:
+                color = 'black'
+            elif o.grid[y][x] == white:
+                color = 'white'
+            elif o.grid[y][x] == legal:
+                end_flag = False
+                if o.player != ai_player:
+                    color = '#3498db'
+                    legal_buttons.append(tkinter.Button(canvas, image=pixel_virtual, width=rect_size - circle_offset * 2, height=rect_size - circle_offset * 2, bg=color, text=str(y) + '_' + str(x)))
+                    legal_buttons[-1].bind('<ButtonPress>', get_coord)
+                    legal_buttons[-1].place(y=offset_y + rect_size * y, x=offset_x + rect_size * x)
+                continue
+            canvas.create_oval(offset_x + rect_size * x + circle_offset, offset_y + rect_size * y + circle_offset, offset_x + rect_size * (x + 1) - circle_offset, offset_y + rect_size * (y + 1) - circle_offset, outline='black', width=2, fill=color, tag=str(y) + '_' + str(x))
+    if o.player == ai_player:
+        app.after(10, ai)
+
+canvas.place(y=0, x=0)
+o.check_legal()
+show_grid()
+app.mainloop()
