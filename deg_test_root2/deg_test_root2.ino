ba78be68ebfa48af19899ccba43b33e0e98c0ce0
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2022-08-25 21:08:22 +0900

    update adj

--- deg_test_root2/deg_test_root2.ino
+++ deg_test_root2/deg_test_root2.ino
@@ -5,20 +5,19 @@
 #define ARM_ROOT_ZERO 7700
 
 void setup() {
+  Serial.begin(115200);
   Wire.begin();
   Wire.setClock(100000);
-  Serial.begin(115200);
-}
-
-void receive() {
-  Serial.println("receive");
-  Serial.print((char)Wire.read());
+  Serial.println("start!");
 }
 
 void loop() {
+  Serial.print((int)Serial.available());
   if (Serial.available()) {
-    Serial.read();
+    Serial.println("\n");
+    Serial.println((char)Serial.read());
     Wire.requestFrom(8, 19);
+    Serial.println("requested");
     for (int i = 0; i < 19; ++i)
       Serial.print((char)Wire.read());
   }
